5b4a185acbc3b9ed49d7f513d83350ad37ef4b96
Rômulo Brito de Farias <fariasbrito.romulo@gmail.com>
2022-03-15 21:27:33 -0300

    scripts adaptados

---
+++ debian-scripts/pinstall.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# install apps, tools and appearance
+apt install pcmanfm neovim python3-neovim git tmux filezilla \
+	papirus-icon-theme breeze-icon-theme breeze-cursor-theme unrar \
+    i3 i3blocks i3status i3lock \
+	suckless-tools xbacklight feh conky lxappearance rofi ssh gsimplecal
+
+# vim-plug vim
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# vim-plug nvim
+#curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+#  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# pacotes necessários para rodar coc.nvim
+#apt install nodejs
+#npm install eslint
+#pip3 install flake8 pylint
