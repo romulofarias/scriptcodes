996e23efba7c81a3a7d83d4a86e6df91c36c256f
Rômulo Brito de Farias <fariasbrito.romulo@gmail.com>
2022-05-10 19:10:45 -0300

    incrementando script original

--- qemu-kvm/mkvm.sh
+++ qemu-kvm/mkvm.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # Script para criar máquina virtual com KVM e libvirt
 
-NOME=teste #TODO: receber nome da VM via prompt (var $1)
+NOME="$1" #TODO: receber nome da VM via prompt (var $1)
 VCPUS=1
 RAM=1024
 MAC=52:54:00:00:00:01 ##TODO: receber MAC via prompt (var $2)
 DEBIAN_VERSION=bullseye
-LOCATION='http://deb.debian.org/debian/dists/"$DEBIAN_VERSION"/main/installer-amd64'
+LOCATION="http://deb.debian.org/debian/dists/${DEBIAN_VERSION}/main/installer-amd64"
 #---Não alterar a partir daqui
-virt-install --name=$NOME --vcpus=$VCPUS --ram $RAM \
-  --disk path=$HOME/kvms/$NOME,bus=virtio,cache=none \
-  --network bridge=br-kvm,model=virtio,mac=$MAC --accelerate \
-  --graphics none --extra-args="console=ttyS0,115200" \
-  --location=$LOCATION
+if [ -z "$1" ]; then
+    echo "Erro! Nome da máquina vazio"
+    exit 1
+else 
+    virt-install --name=$NOME --vcpus=$VCPUS --ram $RAM \
+        --disk path=/home/romulo/kvms/$NOME,bus=virtio,cache=none \
+        --network bridge=br-kvm,model=virtio,mac=$MAC --accelerate \
+        --graphics none --extra-args="console=ttyS0,115200" \
+        --location=$LOCATION
+fi
