14fbbed61df27d51a80ee31da1baa45201bcb3c3
Rômulo Brito de Farias <fariasbrito.romulo@gmail.com>
2024-05-07 18:12:46 -0300

    update!

--- debian-scripts/pinstall.sh
+++ debian-scripts/pinstall.sh
@@ -4,7 +4,7 @@ apt install pcmanfm neovim python3-neovim git tmux filezilla \
 	papirus-icon-theme breeze-icon-theme breeze-cursor-theme rar unrar zip unzip \
     i3 i3blocks i3status i3lock \
 	suckless-tools xbacklight feh conky lxappearance rofi ssh gsimplecal mpv \
-    youtube-dl blueman celluloid
+    youtube-dl blueman celluloid network-manager imagemagick
 
 # C/C++
 # apt install clang clang-format
